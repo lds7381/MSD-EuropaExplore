7d4d72066efbc9452b1ac5e4a6ce9ca324dc961f
Liam Sullivan <lds7381@rit.edu>
2023-04-12 15:12:05 -0400

    Transmission for uart for sensors completed

--- EuropaOS/Core/Inc/uart.h
+++ EuropaOS/Core/Inc/uart.h
@@ -9,11 +9,13 @@
 #define INC_UART_H_
 
 #include "stm32l4xx.h"
+#include "VASensors.h"
 #include "main.h"
 #include <stdio.h>
 
 #define transmit_timeout (100)
 
+void transmit_va_data (UART_HandleTypeDef *uart, uint8_t *sensor_data, int size);
 void print(UART_HandleTypeDef *uart, char *msg, int size);
 
 #endif /* INC_UART_H_ */
--- EuropaOS/Core/Src/uart.c
+++ EuropaOS/Core/Src/uart.c
@@ -7,6 +7,10 @@
 
 #include "uart.h"
 
+void transmit_va_data (UART_HandleTypeDef *uart, uint8_t *sensor_data, int size) {
+	HAL_UART_Transmit(uart, (uint8_t *)sensor_data, sizeof(size), transmit_timeout);
+}
+
 void print(UART_HandleTypeDef *uart, char *msg, int size) {
 	HAL_UART_Transmit(uart, (uint8_t *)msg, size, transmit_timeout);
 }
