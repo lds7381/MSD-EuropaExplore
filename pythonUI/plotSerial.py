91a939e1c131a794e0e1dbbe2749a3dbd3cf1730
Joseph <josephattias@outlook.com>
2023-04-28 01:36:22 -0400

    stuff for csv and plot cleanup

--- pythonUI/plotSerial.py
+++ pythonUI/plotSerial.py
@@ -1,16 +1,14 @@
-import time
+import datetime
 import serial
 import matplotlib.pyplot as plt
-import numpy as np
 import matplotlib.animation as animation
 from tkinter import * 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from PIL import ImageTk,Image
 from tkinter import filedialog
-from multiprocessing import Process
-import threading 
 import glob, os
+import csv
 
 # Reference for protocol interpretation: https://docs.google.com/document/d/17hC0oAoF7pTGZNYeh3ka6t4bvLvzr4SShMjbHldEPO4/edit
 
@@ -34,10 +32,19 @@ def resizeImage(img, newWidth, newHeight):
 # Tkinter class to hold everything 
 class window():
 
-    def __init__(self, container):
+    def __init__(self, container, csvFileName):
         super().__init__()
         container.title("Europa Explorer Interface")
-        container.minsize(640,600)
+        w = 1200
+        h = 800
+        # get screen width and height
+        ws = root.winfo_screenwidth() # width of the screen
+        hs = root.winfo_screenheight() # height of the screen
+        # calculate x and y coordinates for the Tk root window
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+        container.minsize(w,h)
+        container.geometry('%dx%d+%d+%d' % (w, h, x,y))
         # self.wm_iconbitmap('icon.ico')
 
         # Prepare data for capture 
@@ -58,18 +65,19 @@ class window():
         # User selects folders for storage 
         self.data_folder = filedialog.askdirectory(title='folder for sensor data')
         self.images_folder = filedialog.askdirectory(title='folder for camera images')
+        self.csv_file_location = self.images_folder + csvFileName
+        print("Data is stored here: ", self.csv_file_location)
 
         # create each frame and add them to the root container 
         data_frame = self.create_plotting_frame(container)
         data_frame.grid(column=0,row=0)
-        print("plots are added")
+        print("***plots are added")
         cont_frame = self.create_middle_frame(container)
         cont_frame.grid(column=1,row=0)
-        print("rover controls added")
+        print("***rover controls added")
 
         # Initial servo position
         self.servo_pos = 5
-
 
         print("Window init complete \n")
 
@@ -78,7 +86,7 @@ class window():
         frame = Frame(container)
 
         filename_bottom = "D:/Documents/_RIT/MSD-EuropaExplore/pythonUI/images/space_shark_by_bojustbo_da5h40c-fullview-1388306213.jpg" # filedialog.askopenfilename(title='open')
-        print(filename_bottom)
+        print("***Loaded bottom image: " , filename_bottom)
         img_front = ImageTk.PhotoImage(Image.open(filename_bottom).resize(size=[300,200]))
         self.panel_front = Label(frame)
         self.panel_front.image = img_front
@@ -87,16 +95,13 @@ class window():
         Label(frame,text="Front Camera").pack(pady=10)
 
         filename_front = "D:/Documents/_RIT/MSD-EuropaExplore/pythonUI/images/th-21406574.jpg" # filedialog.askopenfilename(title='open')
-        print(filename_front)
+        print("***Loaded front image: " , filename_front)
         img_bottom = ImageTk.PhotoImage(Image.open(filename_front).resize(size=[300,200]))
         self.panel_bottom = Label(frame)
         self.panel_bottom.image = img_bottom
         self.panel_bottom.configure(image=img_bottom)
         self.panel_bottom.pack( fill = "both", expand = "yes")
         Label(frame,text="Bottom Camera").pack(pady=10)
-
-        def stop():
-            pass
 
         controlFrame = Frame(frame)
         Button(controlFrame, text="rudder \nleft",  command=lambda:self.servo_command( 1),          height=5,width=10).grid(column=0, row=1)
@@ -107,7 +112,7 @@ class window():
         for widget in controlFrame.winfo_children():
             widget.grid(padx=5, pady=5)
 
-        controlFrame.pack(side="bottom",pady=40)
+        controlFrame.pack(side="bottom",pady=30)
         return frame 
     
     def servo_command(self,direction):
@@ -121,14 +126,14 @@ class window():
 
     def create_plotting_frame(self, container):
         self.sensor_fig = Figure(dpi=100)
-
         self.sensor_axs = self.sensor_fig.subplots(4, 1)
         self.sensorPlotUpdate(self.sensor_axs)
-
         self.sensor_fig.set_figheight(10)
+        self.sensor_fig.tight_layout()
+        self.sensor_fig.subplots_adjust(top=0.95)
         self.sensorDataUpdate("0,0,0,0,0")
         # prepare frames for figure placement 
-        frame = Frame(container, width= 100, height= 800)
+        frame = Frame(container, width= 100, height= 600)
         # Tie the figures with the frames 
         canvas = FigureCanvasTkAgg(self.sensor_fig, master = frame)
         # Place the figures in the container
@@ -155,10 +160,10 @@ class window():
         self.data_TE = self.data_TE[-20:]
 
     def sensorPlotUpdate(self,axs):
-        # axs[0].clear()
-        # axs[1].clear()
-        # axs[2].clear()
-        # axs[3].clear()
+        axs[0].clear()
+        axs[1].clear()
+        axs[2].clear()
+        axs[3].clear()
 
         # print(axs[0])
         # print(axs[1])
@@ -178,10 +183,10 @@ class window():
         # axs[1].plot(self.time_DO,self.data_SA)
         # axs[2].plot(self.time_DO,self.data_PH)
         # axs[3].plot(self.time_DO,self.data_TE)
-        axs[0].grid()
-        axs[1].grid()
-        axs[2].grid()
-        axs[3].grid()
+        axs[0].grid(True)
+        axs[1].grid(True)
+        axs[2].grid(True)
+        axs[3].grid(True)
         # Axes labels 
         axs[0].set_ylabel("Dissolved Oxygen [g/mL]")
         axs[1].set_ylabel("Salinity [ppt]")
@@ -189,37 +194,37 @@ class window():
         axs[3].set_ylabel("Temperature [Celcius]")
         # axs[0].set_xlabel("Time [s]")
         # axs[1].set_xlabel("Time [s]")
-        # axs[2].set_xlabel("Time [s]") # removed 
+        # axs[2].set_xlabel("Time [s]") # removed to not hog space
         axs[3].set_xlabel("Time [s]")
-        # y limit hard bounds 
-        axs[0].set_ylim(0,20)
-        axs[1].set_ylim(0,25)
-        axs[2].set_ylim(1,12)
-        axs[3].set_ylim(0,100)
         # plot test data in figures 
         axs[0].plot(self.time_DO, self.data_DO)
         axs[1].plot(self.time_SA, self.data_SA)
         axs[2].plot(self.time_PH, self.data_PH)
         axs[3].plot(self.time_TE, self.data_TE)
+        # y limit hard bounds 
+        axs[0].set_ylim([0,20])
+        axs[1].set_ylim([0,25])
+        axs[2].set_ylim([1,12])
+        axs[3].set_ylim([0,100])
         # Label last point 
         axs[0].annotate(text=self.data_DO[-1] ,xy=(self.time_DO[-1],self.data_DO[-1]),textcoords='data')
         axs[1].annotate(text=self.data_SA[-1] ,xy=(self.time_SA[-1],self.data_SA[-1]),textcoords='data')
         axs[2].annotate(text=self.data_PH[-1] ,xy=(self.time_PH[-1],self.data_PH[-1]),textcoords='data')
         axs[3].annotate(text=self.data_TE[-1] ,xy=(self.time_TE[-1],self.data_TE[-1]),textcoords='data')
 
+        plt.show()
     def update_photo(self):
         list_of_files = glob.glob(self.images_folder+"\\*")
-        latest_file =  max(list_of_files, key=os.path.getctime).replace("/", "\\") # .replace("\\","\\\\")
-        print(latest_file)
+        latest_file =  max(list_of_files, key=os.path.getctime).replace("/", "\\") 
         try:
             if latest_file == self.last_photo:
                 return
         except:
             pass
         print("New image: ", latest_file)
-        im = Image.open(latest_file)
+        im = Image.open(latest_file).resize(size=[300,200])
         ph = ImageTk.PhotoImage(im)
-        self.panel_front.configure(image=ph)
+        self.panel_bottom.configure(image=ph)
         self.panel_bottom.image = ph
         self.last_photo = latest_file
 
@@ -229,35 +234,38 @@ class window():
             data_data = ser.read_all()
         except Exception as e:
             print("error: ", e)
-            pass
-        
+            pass  
         if data_data.count(b',')==4 :
             data_string = data_data.decode('ascii') #  receive data as a formatted string
-
             print("String Recieved: ", data_string)
             self.sensorDataUpdate(data_string)
             self.sensorPlotUpdate(axs=self.sensor_axs)  # Plot new data frame
-            self.update_photo()
+        self.update_photo()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Code starts running here 
 
 
 # 209700 bps  baud
-ser = serial.Serial("COM4", 209700)         # Establish Serial object with COM port and BAUD rate to match Port/rate
+dt = datetime.datetime.now()
+csvFileName = dt.strftime('%Y_%m_%d_%H%M%S.csv')
+print(csvFileName)
 
 if __name__ == '__main__':
     root = Tk()
-    win = window(root)
-
+    win = window(root,csvFileName)
     print("window created")
     # Now that everything is started, run the capture and animate loop
     # TODO this only works with a single figure. Convert the array of figures to subplots
-    
     # p = Process(target=animation.FuncAnimation, args=(win.sensor_fig, win.animate, 100, (ser,), 25))
     # p.start()
-    ani = animation.FuncAnimation(win.sensor_fig, win.animate, frames=100, fargs=(ser,), interval=10)
-
+        
+    try:
+        ser = serial.Serial("COM4", 209700)         # Establish Serial object with COM port and BAUD rate to match Port/rate
+        ani = animation.FuncAnimation(win.sensor_fig, win.animate, frames=100, fargs=(ser,), interval=25)
+    except Exception as e:
+        print("error: ", e)
+        pass
 
     print("Entering Mainloop")
     root.mainloop()
